8e9dbfa495b9013069d578e3ec02bbf9398b8e96
Darion Kellar <credkellar@gmail.com>
2026-06-24 00:49:10 -0700

    Add SubvocalDriver class with tension index method

---
+++ drivers/neural/emg_somatic/stream.py
@@ -0,0 +1,5 @@
+class SubvocalDriver:
+    def get_tension_index(self) -> float:
+        # Mocking laryngeal muscle tension sensor
+        # High value = strong subvocal cognitive intent
+        return 0.45 
